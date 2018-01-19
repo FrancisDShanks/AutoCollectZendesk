d978492956c1cf2afb653929de241be0f7aa88e2
unknown <tctcdtc@gmail.com>
2018-01-19 14:05:26 +0800

    modify license info

--- auto_zendesk.py
+++ auto_zendesk.py
@@ -1,24 +1,24 @@
 # -*- coding: utf-8 -*-
 """
-   Copyright 2018 Francis Xufan Du - BEYONDSOFT INC.
+Copyright 2018 Francis Xufan Du - BEYONDSOFT INC.
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-       http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 
-   Created on Thu Jan 18 14:06:16 2018
+Created on Thu Jan 18 14:06:16 2018
 
-	@author: Francis Xufan Du - BEYONDSOFT INC.
-	@email: duxufan@beyondsoft.com xufan.du@gmail.com
-	@Version: 0.1-Beta
+@author: Francis Xufan Du - BEYONDSOFT INC.
+@email: duxufan@beyondsoft.com xufan.du@gmail.com
+@Version: 0.1-Beta
 """
 
 #core mods
